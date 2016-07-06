0cbc35b50d51e66ce2a17a7ad27508b71b09029a
Chance Snow <git@chancesnow.me>
2016-07-06 00:33:34 -0700

    Implement abstract methods

---
+++ Project1/apptbook/src/main/java/edu/pdx/cs410J/chances/AppointmentBook.java
@@ -0,0 +1,29 @@
+package edu.pdx.cs410J.chances;
+
+import edu.pdx.cs410J.AbstractAppointmentBook;
+
+import java.util.Collection;
+
+/**
+ * @author chancesnow
+ */
+public class AppointmentBook extends AbstractAppointmentBook<Appointment>
+{
+    @Override
+    public String getOwnerName()
+    {
+        return null;
+    }
+
+    @Override
+    public Collection<Appointment> getAppointments()
+    {
+        return null;
+    }
+
+    @Override
+    public void addAppointment(Appointment appointment)
+    {
+
+    }
+}
