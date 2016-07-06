9009061b28977c83ce6a7df9350a0bf8a4eecdde
Chance Snow <git@chancesnow.me>
2016-07-06 01:38:01 -0700

    Refactor Appointment unit tests

--- Project1/apptbook/src/test/java/edu/pdx/cs410J/chances/AppointmentTest.java
+++ Project1/apptbook/src/test/java/edu/pdx/cs410J/chances/AppointmentTest.java
@@ -12,52 +12,53 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 public class AppointmentTest {
 
+  private final String DESCRIPTION = "Foobar";
+
   @Test
   public void initiallyDescriptionIsNull() {
-    Appointment appointment = new Appointment();
+    Appointment appointment = new Appointment(DESCRIPTION);
     assertThat(appointment.getDescription(), is(nullValue()));
   }
 
   @Test
   public void initiallyGetBeginTimeReturnsNull() {
-    Appointment appointment = new Appointment();
+    Appointment appointment = new Appointment(DESCRIPTION);
     assertThat(appointment.getBeginTime(), is(nullValue()));
   }
 
   @Test
   public void initiallyGetEndTimeReturnsNull() {
-    Appointment appointment = new Appointment();
+    Appointment appointment = new Appointment(DESCRIPTION);
     assertThat(appointment.getEndTime(), is(nullValue()));
   }
 
   @Test
   public void initiallyGetBeginTimeStringReturnsNull() {
-    Appointment appointment = new Appointment();
+    Appointment appointment = new Appointment(DESCRIPTION);
     assertThat(appointment.getBeginTimeString(), is(nullValue()));
   }
 
   @Test
   public void initiallyGetEndTimeStringReturnsNull() {
-    Appointment appointment = new Appointment();
+    Appointment appointment = new Appointment(DESCRIPTION);
     assertThat(appointment.getEndTimeString(), is(nullValue()));
   }
 
   @Test
   public void canCreateAppointmentWithDescription() {
-    final String DESCRIPTION = "Foobar";
     Appointment appointment = new Appointment(DESCRIPTION);
     assertThat(appointment.getDescription(), is(equalTo(DESCRIPTION)));
   }
 
   @Test
   public void setBeginTimeAcceptsADateObject() {
-    Appointment appointment = new Appointment();
+    Appointment appointment = new Appointment(DESCRIPTION);
     appointment.setBeginTime(new Date());
   }
 
   @Test
   public void setEndTimeAcceptsADateObject() {
-    Appointment appointment = new Appointment();
+    Appointment appointment = new Appointment(DESCRIPTION);
     appointment.setEndTime(new Date());
   }
 }
