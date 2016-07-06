533ebe46362ce6124c107a9e432a60b5b06f6646
Chance Snow <git@chancesnow.me>
2016-07-06 01:27:03 -0700

    Implement appointment book

--- Project1/apptbook/src/main/java/edu/pdx/cs410J/chances/AppointmentBook.java
+++ Project1/apptbook/src/main/java/edu/pdx/cs410J/chances/AppointmentBook.java
@@ -2,28 +2,54 @@ package edu.pdx.cs410J.chances;
 
 import edu.pdx.cs410J.AbstractAppointmentBook;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
  * @author chancesnow
  */
-public class AppointmentBook extends AbstractAppointmentBook<Appointment>
-{
-    @Override
-    public String getOwnerName()
+public class AppointmentBook extends AbstractAppointmentBook<Appointment> {
+    private String owner;
+    private ArrayList<Appointment> appointments;
+
+    /**
+     * Construct a new appointment book given its owner.
+     *
+     * @param owner The book's owner
+     */
+    public AppointmentBook(String owner)
     {
-        return null;
+        this.owner = owner;
+        this.appointments = new ArrayList<>();
     }
 
+    /**
+     * Get the owner's name.
+     *
+     * @return The owner's name
+     */
     @Override
-    public Collection<Appointment> getAppointments()
-    {
-        return null;
+    public String getOwnerName() {
+        return owner;
     }
 
+    /**
+     * Get the appointments added to this appointment book.
+     *
+     * @return Collection of appointments
+     */
     @Override
-    public void addAppointment(Appointment appointment)
-    {
+    public Collection<Appointment> getAppointments() {
+        return appointments;
+    }
 
+    /**
+     * Add an appointment to this appointment book
+     *
+     * @param appointment Appointment to add
+     */
+    @Override
+    public void addAppointment(Appointment appointment) {
+        this.appointments.add(appointment);
     }
 }
