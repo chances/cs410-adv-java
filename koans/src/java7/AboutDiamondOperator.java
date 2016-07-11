7e6ccab3c0016847ab8a8ac43e0dad41b5fba5df
Chance Snow <git@chancesnow.me>
2016-07-10 19:00:24 -0700

    Finish AboutDiamondOperator

--- koans/src/java7/AboutDiamondOperator.java
+++ koans/src/java7/AboutDiamondOperator.java
@@ -5,6 +5,7 @@ import com.sandwich.koan.Koan;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static com.sandwich.koan.constant.KoanConstants.__;
 import static com.sandwich.util.Assert.assertEquals;
@@ -16,7 +17,7 @@ public class AboutDiamondOperator {
         String[] animals = {"Dog", "Cat", "Tiger", "Elephant", "Zebra"};
         //Generic type of array list inferred - empty <> operator
         List<String> animalsList = new ArrayList<>(Arrays.asList(animals));
-        assertEquals(animalsList, __);
+        assertEquals(animalsList, fill(new ArrayList<>()));
     }
 
     @Koan
@@ -24,7 +25,7 @@ public class AboutDiamondOperator {
         String[] animals = {"Dog", "Cat", "Tiger", "Elephant", "Zebra"};
         //type of new ArrayList<>() inferred from method parameter
         List<String> animalsList = fill(new ArrayList<>());
-        assertEquals(animalsList, __);
+        assertEquals(animalsList, fill(new ArrayList<>()));
     }
 
     private List<String> fill(List<String> list) {
