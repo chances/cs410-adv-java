1cf6ae4066a8ece7cfa09daa43d386a6e7558500
Chance Snow <git@chancesnow.me>
2016-07-12 22:16:04 -0700

    Add TextDumper and TextParser classes

---
+++ Project2/apptbook/src/main/java/edu/pdx/cs410J/chances/TextDumper.java
@@ -0,0 +1,18 @@
+package edu.pdx.cs410J.chances;
+
+import edu.pdx.cs410J.AbstractAppointmentBook;
+import edu.pdx.cs410J.AppointmentBookDumper;
+
+import java.io.IOException;
+
+/**
+ * @author chancesnow
+ */
+public class TextDumper implements AppointmentBookDumper
+{
+    @Override
+    public void dump(AbstractAppointmentBook abstractAppointmentBook) throws IOException
+    {
+
+    }
+}
---
+++ Project2/apptbook/src/main/java/edu/pdx/cs410J/chances/TextParser.java
@@ -0,0 +1,17 @@
+package edu.pdx.cs410J.chances;
+
+import edu.pdx.cs410J.AbstractAppointmentBook;
+import edu.pdx.cs410J.AppointmentBookParser;
+import edu.pdx.cs410J.ParserException;
+
+/**
+ * @author chancesnow
+ */
+public class TextParser implements AppointmentBookParser
+{
+    @Override
+    public AbstractAppointmentBook parse() throws ParserException
+    {
+        return null;
+    }
+}
