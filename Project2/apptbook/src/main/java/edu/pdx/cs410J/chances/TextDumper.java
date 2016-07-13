a185af2f4d3f9e541a40c46ee4f347a5a6be826b
Chance Snow <git@chancesnow.me>
2016-07-12 23:44:50 -0700

    Begin implementing appt book dumper
    Take a dump, appointments!

--- Project2/apptbook/src/main/java/edu/pdx/cs410J/chances/TextDumper.java
+++ Project2/apptbook/src/main/java/edu/pdx/cs410J/chances/TextDumper.java
@@ -3,16 +3,45 @@ package edu.pdx.cs410J.chances;
 import edu.pdx.cs410J.AbstractAppointmentBook;
 import edu.pdx.cs410J.AppointmentBookDumper;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @author chancesnow
  */
 public class TextDumper implements AppointmentBookDumper
 {
-    @Override
-    public void dump(AbstractAppointmentBook abstractAppointmentBook) throws IOException
-    {
+    File file;
 
+    /**
+     * Construct a new appt book dumper (writer) given a file path
+     *
+     * @param filePath The file path
+     */
+    public TextDumper(String filePath)
+    {
+        file = new File(filePath);
+
+        // Exceptional state when file is a directory
+        if (file.isDirectory()) {
+            file = null;
+        }
+    }
+
+    /**
+     * Dump (overwrite) an appointment book to a file
+     *
+     * @param appointmentBook The appointment book to dump (write)
+     * @throws IOException
+     */
+    @Override
+    public void dump(AbstractAppointmentBook appointmentBook) throws IOException
+    {
+        if (file != null) {
+            PrintWriter pw = new PrintWriter(file);
+            pw.println("appointment book");
+            pw.close();
+        }
     }
 }
