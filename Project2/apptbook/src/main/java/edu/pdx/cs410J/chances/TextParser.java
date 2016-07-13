9a493dc148ded4ca9dbfc51d929e17f51e0991bb
Chance Snow <git@chancesnow.me>
2016-07-12 23:28:33 -0700

    Begin implementing appt book parser

--- Project2/apptbook/src/main/java/edu/pdx/cs410J/chances/TextParser.java
+++ Project2/apptbook/src/main/java/edu/pdx/cs410J/chances/TextParser.java
@@ -4,7 +4,7 @@ import edu.pdx.cs410J.AbstractAppointmentBook;
 import edu.pdx.cs410J.AppointmentBookParser;
 import edu.pdx.cs410J.ParserException;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * @author chancesnow
@@ -22,9 +22,29 @@ public class TextParser implements AppointmentBookParser
         }
     }
 
+    /**
+     * Parse an appointment book from a file.
+     *
+     * @return Parsed appointment book
+     * @throws ParserException
+     */
     @Override
     public AbstractAppointmentBook parse() throws ParserException
     {
+        if (file != null) {
+            String line;
+
+            try (
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(new FileInputStream(file))
+                    )
+            ) {
+                line = br.readLine();
+            } catch (IOException ex) {
+                throw new ParserException(ex.getMessage(), ex);
+            }
+        }
+
         return null;
     }
 }
