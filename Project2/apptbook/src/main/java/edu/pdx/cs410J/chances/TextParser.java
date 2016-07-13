5afd7d6884b996be0a0569541f711d479f9e6c32
Chance Snow <git@chancesnow.me>
2016-07-12 22:40:21 -0700

    Add constructor accepting file path

--- Project2/apptbook/src/main/java/edu/pdx/cs410J/chances/TextParser.java
+++ Project2/apptbook/src/main/java/edu/pdx/cs410J/chances/TextParser.java
@@ -4,11 +4,24 @@ import edu.pdx.cs410J.AbstractAppointmentBook;
 import edu.pdx.cs410J.AppointmentBookParser;
 import edu.pdx.cs410J.ParserException;
 
+import java.io.File;
+
 /**
  * @author chancesnow
  */
 public class TextParser implements AppointmentBookParser
 {
+    private File file;
+
+    public TextParser(String filePath)
+    {
+        file = new File(filePath);
+
+        if (!file.exists() || !file.isDirectory()) {
+            file = null;
+        }
+    }
+
     @Override
     public AbstractAppointmentBook parse() throws ParserException
     {
